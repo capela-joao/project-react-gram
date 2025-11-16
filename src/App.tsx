c1bc0238e134e0f62c085a004586a695f1471efd
capela-joao <joaopaulo1060@hotmail.com>
2025-11-16 14:29:29 -0300

    Estilizando navbar+footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home/home';
-import About from './pages/About/about';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Navbar from './assets/components/Navbar/Navbar';
+import Footer from './assets/components/Footer/Footer';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     </>
   );
