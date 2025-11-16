fe1a19db6b290f0f21f9f07f5ac7f333e4046395
capela-joao <joaopaulo1060@hotmail.com>
2025-11-16 13:02:51 -0300

    Configurando ESLint e Prettier + Configuração React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home/home';
+import About from './pages/About/about';
 
 function App() {
-
   return (
     <>
-      <h1>MiniBlog</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
